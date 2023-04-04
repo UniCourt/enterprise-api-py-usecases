34fdc5a39cb174c2c61dfcd6e753f00ad43f3a69
Santhosh K <santhoshk@unicourt.com>
2023-04-04 11:59:59 +0530

    Added usecase Identify top case type for a law firm

---
+++ identify_top_case_type_for_law_firm.py
@@ -0,0 +1,36 @@
+import unicourt
+from unicourt import *
+from prettytable import PrettyTable
+
+# Get CLIENT_ID and CLIENT_SECRET from your account
+unicourt.CLIENT_ID = "q7pmyLuoJBDG1DZ8r1AzN48ywLLUrsXn"
+unicourt.CLIENT_SECRET = "advnPwNpHI9KBPqt5pI8K6tLjBUJ272c9OV9F-rnkuHNbRs4KOI1T6CeMn50sOkF"
+Authentication.generate_new_token()
+
+# Retrieve the normLawFirmId value for Kirkland & Ellis
+normalized_law_firms, status_code = LawFirmAnalytics.search_normalized_law_firms(
+    q="name:(Kirkland & Ellis)")
+
+
+normalized_law_firm_id = ""
+# Lets pic normLawFirmId value NORGHsUK4FHHEdEtkh, which is associated
+# with the name "Kirkland & Ellis L.L.P." from the above response
+
+for normalized_law_firm in normalized_law_firms.norm_law_firm_search_result_array:
+    if normalized_law_firm.name == "KIRKLAND & ELLIS L.L.P.":
+        normalized_law_firm_id = normalized_law_firm.norm_law_firm_id
+        print(normalized_law_firm_id)
+        break
+
+# Pass normalized_law_firm_id value to CaseAnalytics.get_case_count_analytics_by_case_type
+case_count_analytics_by_case_types, status_code = CaseAnalytics.get_case_count_analytics_by_case_type(
+    q='normLawFirmId:"NORGHsUK4FHHEdEtkh"', page_number=1)
+
+# From the above response lets print top 20 case count and case type for Kirkland and Ellis
+for case_object in case_count_analytics_by_case_types.results[:10]:
+    print(
+        f"CaseType: {case_object.case_type.area_of_law}", " - ", f"CaseCount:  {case_object.case_count}")
+
+
+# Invalidate the generated access token
+Authentication.invalidate_token()
